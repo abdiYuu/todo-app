b0561b93a02065401cd4ffc27115930618bc1fb7
Abdi Yusuf <abdiyuu7@gmail.com>
2021-07-05 13:01:27 -0600

    created project class

--- src/model.js
+++ src/model.js
@@ -1,14 +1,33 @@
 class User {
-	constructor(todos, projects) {
-		this.todos = todos;
+	constructor(tasks, projects) {
+		this.tasks = tasks;
 		this.projects = projects;
 	}
-	todoCount() {
-		return `You have ${this.todos.length} tasks and ${this.projects.length} projects to do`;
+	taskCount() {
+		return `You have ${this.tasks.length} tasks and ${this.projects.length} projects to do`;
+	}
+}
+
+class Project {
+	constructor(title, description, dueDate, priority, notes, checklist) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.notes = notes;
+		this.checklist = checklist;
+		this.tasks = [];
+	}
+	addTask(task) {
+		this.tasks.push(task);
+	}
+
+	taskCount() {
+		return `This project has ${this.tasks.length} tasks left`;
 	}
 }
 	
-class Todo {
+class Task {
 	constructor(title, description, dueDate, priority, notes, checklist) {
 		this.title = title;
 		this.description = description;
@@ -20,11 +39,17 @@ class Todo {
 	daysRemaining() {
 	}
 }
+let abdi = new User([
+	new Task('brush teeth', '', '3pm', '1', '', ''),
+	new Task('wash dishes', '', '2pm', '2', '', ''),
+	new Task('attend appt', '', '3:20pm', '3', '', ''),
+], [
+	new Project('todo-app', '', 'july 7', '5', '', '')
+]);
 
-let abdi = new User(['brush teeth', 'wash dishes', 'attend appt'], ['todo-app']);
-console.log(abdi.todoCount())
-console.log(abdi.todos);
-console.log(abdi.projects);
 
-
-
+console.log(abdi.taskCount())
+console.table(abdi.tasks);
+console.table(abdi.projects);
+abdi.projects[0].addTask(new Task('create model module', '', 'today', '5', '', ''));
+console.table(abdi.projects[0].tasks)
