f829bb7e1761bbb5bfc6b357573fa9e354192d2c
Abdi Yusuf <abdiyuu7@gmail.com>
2021-07-11 19:50:50 -0600

    removed switchtab, added tab switching conditional to displayproject

--- src/view.js
+++ src/view.js
@@ -11,13 +11,66 @@ const tabs = document.querySelector('.projects');
 taskbtn.addEventListener('click', displayTask);
 projbtn.addEventListener('click', displayProject);
 
-let projects = []; //placeholder to test tab functionality
+let projects = []; //placeholder to test tab functionality;
+let tasks = []; //placeholder for tasks;
 
 function makeProject() {
 	let proj = new Project('test', 'test', 'test', 'test', 'test', 'test');
 	projects.push(proj);
 	return proj;
 }
+
+function makeProjectTab() {
+        const container = document.querySelector('.container');
+        const title = document.createElement('h1');
+        const description = document.createElement('p');
+        const dueDate = document.createElement('p');
+        const priority = document.createElement('p');
+        const notes = document.createElement('p');
+        const tasks = document.createElement('div');
+        tasks.classList.add('sub-container');
+
+        const projtaskbtn = document.createElement('button');
+        projtaskbtn.classList.add('.btn_proj-task');
+        projtaskbtn.addEventListener('click', displayTask);
+        tasks.appendChild(projtaskbtn);
+
+        container.appendChild(title);
+        container.appendChild(description);
+        container.appendChild(dueDate);
+        container.appendChild(priority);
+        container.appendChild(notes);
+        container.appendChild(tasks);
+
+        return {title, description, dueDate, priority, notes, tasks};
+}
+
+function displayProject(e) {
+	if (e.target.id) {
+		let proj = projects[e.target.id]
+	} else {		
+		let proj = makeProject();
+	}
+
+        let {title, description, dueDate, priority, notes, tasks} = makeProjectTab();
+        title.innerText = proj.title;
+        description.innerText = proj.description;
+        dueDate.innerText = proj.dueDate;
+        priority.innerText = proj.priority;
+        notes.innerText = proj.notes;
+        tasks.innerText = proj.tasks;
+
+        const tab = document.createElement('button');
+        tab.innerText = proj.title;
+        tab.id = projects.indexOf(proj);
+
+        tab.addEventListener('click', displayProject);
+        tabs.appendChild(tab);
+
+}
+
+
+
 
 function makeTaskCard() {
 	const card = document.createElement('div');
@@ -42,52 +95,4 @@ function displayTask() {
 	description.innerText = task.description;
 	dueDate.innerText = task.dueDate;
 
-}
-
-function makeProjectTab() {
-	const container = document.querySelector('.container');
-        const title = document.createElement('h1');
-        const description = document.createElement('p');
-        const dueDate = document.createElement('p');
-	const priority = document.createElement('p');
-	const notes = document.createElement('p');
-	const tasks = document.createElement('div');
-	tasks.classList.add('sub-container');
-
-	const projtaskbtn = document.createElement('button');
-	projtaskbtn.classList.add('.btn_proj-task');
-	projtaskbtn.addEventListener('click', displayTask);
-	tasks.appendChild(projtaskbtn);
-
-	container.appendChild(title);
-	container.appendChild(description);
-	container.appendChild(dueDate);
-	container.appendChild(priority);
-	container.appendChild(notes);
-	container.appendChild(tasks);
-	return {title, description, dueDate, priority, notes, tasks};
-}
-
-function displayProject(proj) {
-	projects.push(proj);
-	let {title, description, dueDate, priority, notes, tasks} = makeProjectTab();
-	title.innerText = proj.title;
-	description.innerText = proj.description;
-	dueDate.innerText = proj.dueDate;
-	priority.innerText = proj.priority;
-	notes.innerText = proj.notes;
-	tasks.innerText = proj.tasks;
-	
-	const tab = document.createElement('button');
-	tab.innerText = proj.title;
-	tab.id = projects.indexOf(proj);
-
-	tab.addEventListener('click', switchTab);
-	tabs.appendChild(tab);
-
-}
-
-function switchTab(e) {
-	document.querySelector('.container').innerHTML = '';
-	displayProject(projects[e.target.id]);
 }
