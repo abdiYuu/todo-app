a35add7a97e686647beebab5cc7c45d30c8ccfc1
Abdi Yusuf <abdiyuu7@gmail.com>
2021-07-11 23:15:10 -0600

    redid project. smoothed out project creation

--- src/view.js
+++ src/view.js
@@ -9,97 +9,36 @@ const taskbtn = document.querySelector('.btn_task');
 const projbtn = document.querySelector('.btn_proj');
 const tabs = document.querySelector('.projects');
 
-taskbtn.addEventListener('click', displayTask);
-projbtn.addEventListener('click', displayProject);
 
 let projects = []; //placeholder to test tab functionality;
 let tasks = []; //placeholder for tasks;
 
+
+function toggleForm() {
+	const form = document.querySelector('form');
+	form.classList.toggle('visibility');
+	form.reset();
+	form.addEventListener('submit', makeProject);
+}
+
 function makeProject() {
-	let name = prompt('Enter a name');
-	let proj = new Project(name, 'test', 'test', 'test', 'test', 'test');
+	let title = document.getElementById('title').value;
+	let proj = new Project(title, '' ,'', '', '', '');
 	projects.push(proj);
-	return proj;
+	displayProject(proj)
 }
 
-function makeProjectTab() {
-	while(container.children.length > 1) {
-		container.removeChild(container.lastElementChild);
-	}
-
-        const title = document.createElement('h1');
-        const description = document.createElement('p');
-        const dueDate = document.createElement('p');
-        const priority = document.createElement('p');
-        const notes = document.createElement('p');
-        const tasks = document.createElement('div');
-        tasks.classList.add('sub-container');
-
-        const projtaskbtn = document.createElement('button');
-        projtaskbtn.classList.add('.btn_proj-task');
-        projtaskbtn.addEventListener('click', displayTask);
-        tasks.appendChild(projtaskbtn);
-
-        container.appendChild(title);
-        container.appendChild(description);
-        container.appendChild(dueDate);
-        container.appendChild(priority);
-        container.appendChild(notes);
-        container.appendChild(tasks);
-
-        return {title, description, dueDate, priority, notes, tasks};
+function displayProject(proj) {
+	const projtitle = document.createElement('h1');
+	projtitle.innerText = proj.title;
+	
+	container.appendChild(projtitle);
 }
 
-function displayProject(e) {
-	let proj;
-	let tab;
-	if (e.target.id) {
-		proj = projects[e.target.id]
-	} else {		
-		proj = makeProject();
-		tab = document.createElement('button');
-		tab.innerText = proj.title;
-		tab.id = projects.indexOf(proj);
-
-		tab.addEventListener('click', displayProject);
-		tabs.appendChild(tab);
-
-	}
-
-        let {title, description, dueDate, priority, notes, tasks} = makeProjectTab();
-        title.innerText = proj.title;
-        description.innerText = proj.description;
-        dueDate.innerText = proj.dueDate;
-        priority.innerText = proj.priority;
-        notes.innerText = proj.notes;
-        tasks.innerText = proj.tasks;
-
+function makeTask() {
+	console.log('in progress')
 }
 
+taskbtn.addEventListener('click', makeTask);
+projbtn.addEventListener('click', toggleForm);
 
-
-
-function makeTaskCard() {
-	const card = document.createElement('div');
-	const title = document.createElement('h1');
-	const description = document.createElement('p');
-	const dueDate = document.createElement('p');
-
-	card.appendChild(title);
-	card.appendChild(description);
-	card.appendChild(dueDate);
-
-	taskbar.appendChild(card);
-
-return {title, description, dueDate};
-
-}
-function displayTask() {
-	let task = new Task('test', 'a test task', 'now', '1', 'n/a', 'n/a');
-	let {title, description, dueDate} = makeTaskCard();
-
-	title.innerText = task.title;
-	description.innerText = task.description;
-	dueDate.innerText = task.dueDate;
-
-}
