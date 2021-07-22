ae4712f9a778ef3ec0ba5bcf158220996c7591d8
Abdi Yusuf <abdiyuu7@gmail.com>
2021-07-21 21:37:42 -0600

    added JSON.parse/stringify where localStorage is accessed

--- src/model.js
+++ src/model.js
@@ -4,6 +4,49 @@ switch(true) {
                 user = new User(localStorage);
                 break;
         case(localStorage.getItem('projects')):
+                localStorage.setItem('todos', JSON.stringify([]));
+                user = new User(localStorage);
+                break;
+        case(localStorage.getItem('todos')):
+                localStorage.setItem('projects', JSON.stringify([]));
+                user = new User(localStorage);
+                break;
+        default:
+                localStorage.setItem('projects', JSON.stringify([]));
+                localStorage.setItem('todos', JSON.stringify([]));
+                user = new User(localStorage);
+}
+
+function User(storedInfo) {
+        this.projects = JSON.parse(storedInfo.projects);
+        this.todos = JSON.parse(storedInfo.todos);
+        this.add = function(item) {
+                if(item instanceof Todo) {
+                        this.todos.push(item);
+                        localStorage.setItem('todos', JSON.stringify(this.todos));
+                } else {
+                        this.projects.push(item);
+                        localStorage.setItem('projects', JSON.stringify(this.projects));
+                }
+        }
+        this.remove = function(item) {
+                if(item instanceof Todo) {
+                        let index = this.todos.indexOf(item);
+                        this.todos.splice(index, 1);
+                        localStorage.setItem('todos', JSON.stringify(this.todos));
+                } else {
+                        let index = this.projects.indexOf(item);
+                        this.projects.splice(index, 1);
+                        localStorage.setItem('projects', JSON.stringify(this.projects));
+                }
+        }
+}
+let user;
+switch(true) {
+        case(localStorage.getItem('projects') && localStorage.getItem('todos')):
+                user = new User(localStorage);
+                break;
+        case(localStorage.getItem('projects')):
                 localStorage.setItem('todos', []);
                 user = new User(localStorage);
                 break;
