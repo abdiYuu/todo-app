8fbe274ced07b8daf325f77a5d4fed48b1ba5c87
Abdi Yusuf <abdiyuu7@gmail.com>
2021-07-24 22:07:36 -0600

    -made todoList module IIFE -created makeProject/Todo and deleteProject/Todo functions -fixed conditional in User.add()

--- src/model.js
+++ src/model.js
@@ -1,4 +1,4 @@
-function todoList() {
+let todoList = (function todoList() {
 	'use strict'
 	let user;
 	switch(true) {
@@ -6,43 +6,47 @@ function todoList() {
 			user = new User(localStorage);
 			break;
 		case(localStorage.getItem('projects')):
-			localStorage.setItem('todos', []);
+			localStorage.setItem('todos', JSON.stringify([]));
 			user = new User(localStorage);
 			break;
 		case(localStorage.getItem('todos')):
-			localStorage.setItem('projects', []);
+			localStorage.setItem('projects', JSON.stringify([]));
 			user = new User(localStorage);
 			break;
 		default:
-			localStorage.setItem('projects', []);
-			localStorage.setItem('todos', []);
+			localStorage.setItem('projects', JSON.stringify([]));
+			localStorage.setItem('todos', JSON.stringify([]));
 			user = new User(localStorage);
 	}
 
 	function User(storedInfo) {
-		this.projects = storedInfo.projects;
-		this.todos = storedInfo.todos;
+		this.projects = JSON.parse(storedInfo.projects);
+		this.todos = JSON.parse(storedInfo.todos);
 		this.add = function(item) {
-			if(item instanceof Todo) {
-				this.todos.push(item);
-			} else {
+			if(item instanceof Project) {
 				this.projects.push(item);
+				localStorage.setItem('projects', JSON.stringify(this.projects));
+			} else {
+				this.todos.push(item);
+				localStorage.setItem('todos', JSON.stringify(this.todos));
 			}
 		}
 		this.remove = function(item) {
-			if(item instanceof Todo) {
-				let index = this.todos.indexOf(item);
-				this.todos.splice(index, 1);
-			} else {
+			if(item instanceof Project) {
 				let index = this.projects.indexOf(item);
 				this.projects.splice(index, 1);
+				localStorage.setItem('projects', JSON.stringify(this.projects));
+			} else {
+				let index = this.todos.indexOf(item);
+				this.todos.splice(index, 1);
+				localStorage.setItem('todos', JSON.stringify(this.todos));
 			}
 		}
 	}
 
 	class Todo {
 		constructor(name) {
-			this.name = name;
+			this.name = name; 
 			this.dueDate = 'YYYY/MM/DD';
 			this.setDueDate = function(date) {
 				this.dueDate = date;
@@ -64,13 +68,16 @@ function todoList() {
 			this.todos.splice(index, 1);
 		}
 	}
-	
-	function makeProject() {
+	function makeTodo(name) {
+		let todo = new Todo(name);
+		user.add(todo);
+		return todo;
 	}
-	function makeTodo() {
+	function makeProject(name) {
+		let project = new Project(name);
+		user.add(project);
+		return project;
 	}
-	function deleteProject() {
-	}
-	function deleteTodo() {
-	}
-}
+	return {user, makeTodo, makeProject}
+
+})();
